205bcbf7d94818eeeeb9199c01c09a397e2cbd31
Anubis <github@anubis.email>
2022-06-07 15:33:35 +0200

    button interrupt detected

--- esp/radio/main/main.c
+++ esp/radio/main/main.c
@@ -24,6 +24,9 @@
 #include "periph_wifi.h"
 #include "board.h"
 
+#include "periph_adc_button.h"
+#include "input_key_service.h"
+
 #include "audio_idf_version.h"
 
 #if (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 1, 0))
@@ -36,9 +39,9 @@
 
 static const char *TAG = "ESP32 Internet radio";
 
-
 void configTask(void *args);
 void radioTask(void *args);
+static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_service_event_t *evt, void *ctx);
 
 MessageBufferHandle_t urlMsg;
 TaskHandle_t configTaskHandler, radioTaskHandler;
@@ -103,6 +106,7 @@ void configTask(void *args) {
 }
 
 void radioTask(void *args) {
+    
     char urlBuf[URL_BUF_SIZE];
     radioformat_t radioFormat = RADIOFORMAT_MP3;
     audio_element_handle_t *current_decoder;
@@ -181,6 +185,23 @@ void radioTask(void *args) {
     esp_periph_start(set, wifi_handle);
     periph_wifi_wait_for_connected(wifi_handle, portMAX_DELAY);
 
+    // ESP_LOGI(TAG, "[ 1 ] Initialize peripherals");
+    esp_periph_config_t periph_cfg2 = DEFAULT_ESP_PERIPH_SET_CONFIG();
+    esp_periph_set_handle_t set2 = esp_periph_set_init(&periph_cfg2);
+
+    // ESP_LOGI(TAG, "[ 2 ] Initialize Button peripheral with board init");
+    audio_board_key_init(set2);
+
+    // ESP_LOGI(TAG, "[ 3 ] Create and start input key service");
+    input_key_service_info_t input_key_info[] = INPUT_KEY_DEFAULT_INFO();
+    input_key_service_cfg_t input_cfg = INPUT_KEY_SERVICE_DEFAULT_CONFIG();
+    input_cfg.handle = set2;
+    input_cfg.based_cfg.task_stack = 4 * 1024;
+    periph_service_handle_t input_ser = input_key_service_create(&input_cfg);
+
+    input_key_service_add_key(input_ser, input_key_info, INPUT_KEY_NUM);
+    periph_service_set_callback(input_ser, input_key_service_cb, NULL);
+
     ESP_LOGE(TAG, "I MANAGED TO GET HERE 12");// EVENT LISTENER
     audio_event_iface_cfg_t evt_cfg = AUDIO_EVENT_IFACE_DEFAULT_CFG();
     audio_event_iface_handle_t evt = audio_event_iface_init(&evt_cfg);
@@ -255,5 +276,51 @@ void radioTask(void *args) {
     audio_element_deinit(aac_decoder);
     audio_element_deinit(mp3_decoder);
     esp_periph_set_destroy(set);
+}
+
+static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_service_event_t *evt, void *ctx)
+{
+    ESP_LOGD(TAG, "[ * ] input key id is %d, %d", (int)evt->data, evt->type);
+    const char *key_types[INPUT_KEY_SERVICE_ACTION_PRESS_RELEASE + 1] = {"UNKNOWN", "CLICKED", "CLICK RELEASED", "PRESSED", "PRESS RELEASED"};
+    switch ((int)evt->data) {
+        case INPUT_KEY_USER_ID_REC:
+            ESP_LOGI(TAG, "[ * ] [Rec] KEY %s", key_types[evt->type]);
+            break;
+        case INPUT_KEY_USER_ID_SET:
+            ESP_LOGI(TAG, "[ * ] [SET] KEY %s", key_types[evt->type]);
+            break;
+        case INPUT_KEY_USER_ID_PLAY:
+            ESP_LOGI(TAG, "[ * ] [Play] KEY %s", key_types[evt->type]);
+            break;
+        case INPUT_KEY_USER_ID_MODE:
+            ESP_LOGI(TAG, "[ * ] [MODE] KEY %s", key_types[evt->type]);
+            break;
+        case INPUT_KEY_USER_ID_VOLDOWN:
+            ESP_LOGI(TAG, "[ * ] [Vol-] KEY %s", key_types[evt->type]);
+            break;
+        case INPUT_KEY_USER_ID_VOLUP:
+            ESP_LOGI(TAG, "[ * ] [Vol+] KEY %s", key_types[evt->type]);
+            break;
+        case INPUT_KEY_USER_ID_MUTE:
+            ESP_LOGI(TAG, "[ * ] [MUTE] KEY %s", key_types[evt->type]);
+            break;
+        case INPUT_KEY_USER_ID_CAPTURE:
+            ESP_LOGI(TAG, "[ * ] [CAPTURE] KEY %s", key_types[evt->type]);
+            break;
+        case INPUT_KEY_USER_ID_MSG:
+            ESP_LOGI(TAG, "[ * ] [MSG] KEY %s", key_types[evt->type]);
+            break;
+        case INPUT_KEY_USER_ID_BATTERY_CHARGING:
+            ESP_LOGI(TAG, "[ * ] [BATTERY_CHARGING] KEY %s", key_types[evt->type]);
+            break;
+        case INPUT_KEY_USER_ID_WAKEUP:
+            ESP_LOGI(TAG, "[ * ] [WAKEUP] KEY %s", key_types[evt->type]);
+            break;
+        default:
+            ESP_LOGE(TAG, "User Key ID[%d] does not support", (int)evt->data);
+            break;
+    }
+
+    return ESP_OK;
 }
 
